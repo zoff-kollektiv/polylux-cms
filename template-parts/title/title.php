54151c143bbe19e2597e02eaab8f4f95b4b81739
Gustav Pursche <pursche@posteo.de>
2019-08-20 14:14:31 +0200

    Title: render subtitle

--- template-parts/title/title.php
+++ template-parts/title/title.php
@@ -1,3 +1,13 @@
+<?php
+  $subtitle = get_field('subtitle');
+?>
+
 <h1 class="title">
   <?php the_title(); ?>
+
+  <?php if ($subtitle) : ?>
+    <small class="title__subtitle">
+      <?php echo $subtitle; ?>
+    </small>
+  <?php endif; ?>
 </h1>
