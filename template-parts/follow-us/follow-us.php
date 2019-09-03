3ab0aed0ec2c5b22ed62a2f609a5fac790bfcfa1
Gustav Pursche <pursche@posteo.de>
2019-09-03 13:50:27 +0200

    Button: fix styles for iOS devices

--- template-parts/follow-us/follow-us.php
+++ template-parts/follow-us/follow-us.php
@@ -3,7 +3,6 @@
 $instagram_url = get_field('instagram_url', 'option');
 $twitter_url = get_field('twitter_url', 'option');
 $facebook_url = get_field('facebook_url', 'option');
-
 ?>
 
 <div class="follow-us">
