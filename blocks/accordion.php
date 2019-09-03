40998030164e0ff20fbaeefe057e1a5c939856dd
Gustav Pursche <pursche@posteo.de>
2019-09-03 13:52:56 +0200

    Accordion: fix markup if several layouts are attached

--- blocks/accordion.php
+++ blocks/accordion.php
@@ -6,26 +6,27 @@
         <h2 class="accordion__title"><?php echo get_sub_field('title'); ?></h2>
 
         <?php if (have_rows('content')): ?>
-          <?php while (have_rows('content')):
-              the_row(); ?>
-            <div class="accordion__content">
-              <?php if (get_row_layout() == 'richtext'): ?>
-                <div class="richtext">
-                  <?php the_sub_field('content'); ?>
-                </div>
-              <?php elseif (get_row_layout() == 'formular'): ?>
-                <div class="form">
-                  <?php
-                  $form = get_sub_field('formular');
-                  echo do_shortcode(
-                      '[contact-form-7 id="' . $form[0]->ID . '"]'
-                  );
-                  ?>
-                </div>
-              <?php endif; ?>
-            </div>
-          <?php
-          endwhile; ?>
+          <div class="accordion__content">
+            <?php while (have_rows('content')):
+                the_row(); ?>
+
+                <?php if (get_row_layout() == 'richtext'): ?>
+                  <div class="richtext">
+                    <?php the_sub_field('content'); ?>
+                  </div>
+                <?php elseif (get_row_layout() == 'formular'): ?>
+                  <div class="form">
+                    <?php
+                    $form = get_sub_field('formular');
+                    echo do_shortcode(
+                        '[contact-form-7 id="' . $form[0]->ID . '"]'
+                    );
+                    ?>
+                  </div>
+                <?php endif; ?>
+            <?php
+            endwhile; ?>
+          </div>
         <?php endif; ?>
       </li>
     <?php
